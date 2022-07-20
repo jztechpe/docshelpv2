065469a57ba41d4943624a0c400187fcc2063547
ErickCamiloaga <fercamiloaga@gmail.com>
2022-07-20 14:39:25 -0500

    update docsv2

---
+++ docsv2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  <NavbarUno/>
+  <Sidebar/>
+  </div>
+  
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue'
+import NavbarUno from './components/NavbarUno.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    NavbarUno
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ docsv2/src/components/NavbarUno.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-button v-b-toggle.sidebar-menu><b-icon icon="list"></b-icon></b-button>
+      <b-navbar-brand href="#">Docs</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    </b-navbar>
+  </div>
+</template>
+<script>
+</script>
+
+<style>
+</style>
